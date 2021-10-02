cb95444e8b76f458a64bd26e4ba54f047717d0c6
DunZek <duncanrledesma_drl@yahoo.com.ph>
2021-10-01 23:03:57 -0600

    added data.py

---
+++ data.py
@@ -0,0 +1,37 @@
+import json, datetime, holidays
+
+# Meta data
+with open('meta.json') as f:
+    meta = json.load(f)
+    # For on_message
+    ID_PC = meta["GGA"]["ID_PC"]
+    ID_MB = meta["GGA"]["ID_MB"]
+    ID = str(meta["GGA"]["ID"])
+
+# Messages
+with open('messages.json') as f:
+    messages = json.load(f)
+
+# Class Schedule
+with open("schedule.json") as f:
+    schedule = json.load(f)
+
+# List of holidays
+with open("manual_holidays.json") as f:
+    man_holidays = json.load(f)
+    new_holidays = {}
+    for key in man_holidays:
+        year = man_holidays[key][0]
+        month = man_holidays[key][1]
+        day = man_holidays[key][2]
+        new_holidays[datetime.date(year, month, day)] = key
+    # For automated
+    weekdays = dict.keys(schedule)
+    holiday_dates = [holiday[0].strftime("%x") for holiday in holidays.Canada(years=2021).items()]
+    holiday_dates += [holiday[0].strftime("%x") for holiday in new_holidays.items()]
+    holiday_names = [holiday[1] for holiday in holidays.Canada(years=2021).items()]
+    holiday_names += [holiday[1] for holiday in new_holidays.items()]
+
+# Flags
+with open("flags.json") as f:
+    flags = json.load(f)
